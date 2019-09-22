0969a8a7c1e77242929256522c8476fcbb6c9a52
Trần Võ Văn <van31071996@gmail.com>
2019-09-22 21:12:25 +0700

    tuan2
    tuan2

---
+++ src/Tuan_2/bai_1.java
@@ -0,0 +1,23 @@
+
+package Tuan_2;
+
+ //Thực hành cách hiển thị cửa sổ Windows trong Java//
+import javax.swing.JFrame;
+
+public class bai_1 extends JFrame
+{
+ public bai_1()
+ {
+     super("Lab Bai_1");
+     setDefaultCloseOperation(EXIT_ON_CLOSE);
+      
+ }
+ 
+    public static void main(String[] args) {
+        bai_1 ui =new bai_1();
+        ui.setSize(400, 300);
+        ui.setLocationRelativeTo(null);
+        ui.setVisible(true);
+    }
+   
+}
